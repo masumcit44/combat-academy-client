1254bafe738a61c3caea9ba782431cf862fa7fef
masum <masumcit44@gmail.com>
2023-06-07 11:51:34 +0600

    basic route added

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,17 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import AuthProvider from "./providers/AuthProvider.jsx";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import { router } from "./Routes/Routes.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <AuthProvider>
       <div className="max-w-screen-lg mx-auto">
-        <App />
+        <RouterProvider router={router} ></RouterProvider>
       </div>
     </AuthProvider>
   </React.StrictMode>
